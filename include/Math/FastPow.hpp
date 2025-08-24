83d02fdcb61bc97cb51a4b46b3a9f8e7da1b2c4b
reed <1457882960@qq.com>
2025-08-24 17:26:09 +0800

    为FastPow添加了IntType用以在函数参数不是常规整型时的适配

--- include/Math/FastPow.hpp
+++ include/Math/FastPow.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <bits/stdc++.h>
+template <class T>
+using IntType = std::conditional_t<(sizeof(T) <= 16), T, const T&>;
 
 // 循环实现
 template<class Int = int>
-constexpr Int fastPow(Int base, Int power) noexcept {
+constexpr Int fastPow(IntType<Int> base, IntType<Int> power) noexcept {
     Int ret = 1;
     for (; power; power /= 2, base *= base) {
         if (power % 2) ret *= base;
@@ -13,7 +15,7 @@ constexpr Int fastPow(Int base, Int power) noexcept {
 
 // 带模运算的循环实现
 template<class Int = int>
-constexpr Int fastPow(Int base, Int power, Int mod) noexcept {
+constexpr Int fastPow(IntType<Int> base, IntType<Int> power, IntType<Int> mod) noexcept {
     Int ret = 1;
     base %= mod;
     for (; power; power /= 2, base = base * base % mod) {
