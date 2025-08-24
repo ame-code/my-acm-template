b8d87d5c98ccc25393a0d0d140cd9ac1d2b7a838
reed <1457882960@qq.com>
2025-08-24 17:18:22 +0800

    将FastPow中的>>=1改为了/=2，取消了模板约束

--- include/Math/FastPow.hpp
+++ include/Math/FastPow.hpp
@@ -2,21 +2,21 @@
 #include <bits/stdc++.h>
 
 // 循环实现
-template<std::integral Int = int>
-constexpr Int fastPow(Int base, Int index) noexcept {
+template<class Int = int>
+constexpr Int fastPow(Int base, Int power) noexcept {
     Int ret = 1;
-    for (; index; index >>= 1, base *= base) {
-        if (index & 1) ret *= base;
+    for (; power; power /= 2, base *= base) {
+        if (power & 1) ret *= base;
     }
     return ret;
 }
 
 // 带模运算的循环实现
-template<std::integral Int = int>
+template<class Int = int>
 constexpr Int fastPow(Int base, Int index, Int mod) noexcept {
     Int ret = 1;
     base %= mod;
-    for (; index; index >>= 1, base = base * base % mod) {
+    for (; index; index /= 2, base = base * base % mod) {
         if (index & 1) ret = ret * base % mod;
     }
     return ret;
