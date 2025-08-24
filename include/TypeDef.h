85c89da507dfb094aa28b38da6490eb0aef53954
reed <1457882960@qq.com>
2025-08-24 16:21:46 +0800

    添加TypeDef.h文件，提供部分类型别名的定义

---
+++ include/TypeDef.h
@@ -0,0 +1,11 @@
+#pragma once
+
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned i64;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+template <class T>
+using ref = T&;
+template <class T>
+using cref = const T&;
