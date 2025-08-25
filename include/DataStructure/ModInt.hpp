0a99876f94f383155985899a6d3683664ffded3b
reed <1457882960@qq.com>
2025-08-25 15:47:41 +0800

    修复了ModInt中if属性位置错误、没有默认构造函数、Mod打成MOD的bug

--- include/DataStructure/ModInt.hpp
+++ include/DataStructure/ModInt.hpp
@@ -7,6 +7,7 @@ template <auto Mod = 998244353l, class T = decltype(Mod)>
 struct ModInt
 {
     T num;
+    constexpr ModInt() : ModInt(T{}) {}
     constexpr ModInt(T x) : num(norm(x % Mod)) {}
     constexpr ModInt(cref<ModInt>) = default;
     constexpr ModInt(ModInt&&) noexcept = default;
@@ -14,12 +15,12 @@ struct ModInt
     constexpr ModInt& operator=(ModInt&&) noexcept = default;
 
     constexpr T norm(T x) const {
-        if [[unlikely]] (x < 0) x += Mod;
-        else if [[unlikely]] (x >= Mod) x -= Mod;
+        [[unlikely]] if (x < 0) x += Mod;
+        else [[unlikely]] if (x >= Mod) x -= Mod;
         return x;
     }
     constexpr explicit operator T() const {return num;}
-    constexpr ModInt inv() const {return fastPow(num, MOD - 2);}
+    constexpr ModInt inv() const {return fastPow(num, Mod - 2);}
 
     friend auto& operator>>(std::istream& is, ModInt& modint) {
         T x; is >> x; modint.num = modint.norm(x);
@@ -29,7 +30,7 @@ struct ModInt
         return os << modint.num;
     }
     constexpr ModInt operator-() const {
-        return ModInt(MOD - num);
+        return ModInt(Mod - num);
     }
     constexpr ModInt& operator++() {
         num = norm(num + 1);
@@ -66,7 +67,7 @@ struct ModInt
     }
     template <std::integral Int>
     constexpr ModInt& operator%=(Int other) {
-        return x %= other, *this;
+        return num %= other, *this;
     }
     constexpr ModInt operator+(cref<ModInt> other) const {return ModInt(num) += other;}
     constexpr ModInt operator-(cref<ModInt> other) const {return ModInt(num) -= other;}
