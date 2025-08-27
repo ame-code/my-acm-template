d56caa1e6d8ab4243b694602dff359f69f253dbb
reed <1457882960@qq.com>
2025-08-27 17:16:08 +0800

    添加了最小表示法板子MinLexRotator.hpp

---
+++ include/String/MinLexRotator.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <bits/stdc++.h>
+
+struct MinLexRotator {
+    template<class T>
+    size_t operator()(std::span<T> arr) const {
+        size_t i{}, j{1}, k{0};
+        size_t n = arr.size();
+        while (i < n && j < n && k < n) {
+            if (arr[(i + k) % n] == arr[(j + k) % n])
+                k++;
+            else {
+                if (arr[(i + k) % n] > arr[(j + k) % n]) 
+                    i += k + 1;
+                else
+                    j += k + 1;
+                i += (i == j);
+                k = 0;
+            }
+        }
+        return std::min(i, j);
+    }
+};
