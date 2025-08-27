9ae58e30e525972cc50b450e3ca715122cd30190
reed <1457882960@qq.com>
2025-08-27 17:37:18 +0800

    将传入参数从std::span改回std::string_view

--- include/String/MinLexRotator.hpp
+++ include/String/MinLexRotator.hpp
@@ -3,7 +3,7 @@
 
 struct MinLexRotator {
     template<class T>
-    size_t operator()(std::span<T> arr) const {
+    size_t operator()(std::string_view arr) const {
         size_t i{}, j{1}, k{0};
         size_t n = arr.size();
         while (i < n && j < n && k < n) {
