a3d7af996893ff213e776af29c98ac007b775a0f
reed <1457882960@qq.com>
2025-08-28 13:41:03 +0800

    为Trie添加了查询前缀个数功能

--- include/String/Trie.hpp
+++ include/String/Trie.hpp
@@ -5,10 +5,10 @@ template <size_t Size = 200'000>
 struct Trie
 {
     std::vector<std::array<int, 26>> tree;
-    std::unordered_set<int> exist;
+    std::vector<int> exist;
     size_t cnt;
 
-    Trie() : tree(Size), cnt(0) {}
+    Trie() : tree(Size, std::array<int, 26>{}), exist(Size, 0) cnt(0) {}
 
     void insert(std::string_view s) {
         int p = 0;
@@ -16,16 +16,16 @@ struct Trie
             int c = ch - 'a';
             if (!tree[p][c]) tree[p][c] = ++cnt;
             p = tree[p][c];
+            exist[p]++;
         }
-        exist.insert(p);
     }
-    bool search(std::string_view s) const {
+    int search(std::string_view s) const {
         int p = 0;
         for (int i = 0; i < std::ssize(s); i++) {
             int c = s[i] - 'a';
             if (!tree[p][c]) return false;
             p = tree[p][c];
         }
-        return exist.contains(p);
+        return exist[p];
     }
 };
