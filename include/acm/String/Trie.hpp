493070718e0928ae8c40558e4e26425eb9bd24b6
reed <1457882960@qq.com>
2026-05-20 18:17:04 +0800

    update Trie

--- include/acm/String/Trie.hpp
+++ include/acm/String/Trie.hpp
@@ -1,222 +1,222 @@
 #pragma once
-#include <bits/stdc++.h>
 
-namespace Trie
-{
+#include <array>
+#include <concepts>
+#include <cstddef>
+#include <string_view>
+#include <type_traits>
+#include <utility>
+#include <vector>
+#include "BitTraverser.hpp"
+namespace Trie {
+
+template< typename Node >
+class Nodes {
+	std::vector<Node> nodes_;
+
+protected:
+	Nodes(): nodes_(1, Node{}) {}
+
+	void reserve(std::size_t cap) { nodes_.reserve(cap); }
+
+	std::size_t alloc() {
+		nodes_.emplace_back(Node{});
+		return nodes_.size() - 1;
+	}
+
+	Node& node(std::size_t i) {
+		return nodes_[i];
+	}
+
+	const Node& node(std::size_t i) const {
+		return nodes_[i];
+	}
+};
+
 struct LowerToIndex {
-    int operator()(char ch) const {
-        return ch - 'a';
-    }
+	unsigned char operator()(char c) const {
+		return c - 'a';
+	}
 };
 
-struct UpperToIndex {
-    int operator()(char ch) const {
-        return ch - 'A';
-    }
+template < std::size_t Size >
+struct ASCIINode {
+	std::array<std::size_t, Size> next_{};
+	std::size_t count_ = 0;
 };
 
-struct AlphaToIndex {
-    int operator()(char ch) const {
-        if (std::isupper(ch)) {
-            return ch - 'A';
-        } else {
-            return ch - 'a' + 'A';
-        }
-    }
+struct MaxXorNode {
+	std::array<std::size_t, 2> next_{};
+	std::size_t count_ = 0;
 };
 
-struct AlnumToIndex {
-    int operator()(char ch) const {
-        if (std::isdigit(ch)) {
-            return ch - '0';
-        } else if (std::isupper(ch)) {
-            return ch - 'A' + '0';
-        } else {
-            return ch - 'a' + '0' + 'A';
-        }
-    }
+struct AllXorNode {
+	std::array<std::size_t, 2> next_{};
+	std::size_t count_ = 0;
+	std::size_t xor_val_ = 0;
 };
 
-template <int ASCIISize = 26, class MapFunc = LowerToIndex>
-struct ASCII
-{
-    using Node = std::array<int, ASCIISize>;
+template < std::size_t Size = 26, typename Mapping = LowerToIndex >
+class ASCII : private Nodes<ASCIINode<Size>>, private Mapping {
+	using Base = Nodes<ASCIINode<Size>>;
+	using Base::alloc;
+	using Base::node;
 
-    ASCII(int size): tree(size, Node{}), exist(size, 0), cnt(0), mapFunc(MapFunc{}) {}
+public:
+	ASCII(): Base(), Mapping() {}
+	ASCII(const Mapping& map): Base(), Mapping(map) {}
 
-    void insert(std::string_view s) {
-        int p = 0;
-        for (auto ch : s) {
-            int c = mapFunc(ch);
-            if (!tree[p][c]) tree[p][c] = ++cnt;
-            p = tree[p][c];
-            exist[p]++;
-        }
-    }
+	void insert(std::string_view sv) {
+		std::size_t p = 0;
+		for (char ch : sv) {
+			std::size_t i = (*this)(ch);
+			if (!node(p).next_[i]) {
+				auto next = alloc();
+				node(p).next_[i] = next;
+			}
+			p = node(p).next_[i];
+			node(p).count_++;
+		}
+	}
 
-    int search(std::string_view s) const {
-        int p = 0;
-        for (auto ch : s) {
-            int c = mapFunc(ch);
-            if (!tree[p][c]) return 0;
-            p = tree[p][c];
-        }
-        return exist[p];
-    }
+	std::size_t search(std::string_view sv) const {
+		std::size_t p = 0;
+		for (char ch : sv) {
+			std::size_t i = (*this)(ch);
+			if (!node(p).next_[i]) {
+				return 0;
+			}
+			p = node(p).next_[i];
+		}
 
-private:
-    std::vector<Node> tree;
-    std::vector<int> exist;
-    int cnt;
-    [[no_unique_address]] MapFunc mapFunc;
+		return node(p).count_;
+	}
 };
 
-template <int BitSize = 31>
-struct MaxXor {
-    struct Node {
-        std::array<int, 2> son_idx{};
-        int size{};
-    };
+template < std::integral Int >
+class MaxXor : private Nodes<MaxXorNode>{
+	using Base = Nodes<MaxXorNode>;	
+	using U    = std::make_unsigned_t<Int>;
+public:
+	MaxXor(): Base() {}
 
-    MaxXor(int size): tree(size, Node{}), cnt(0) {}
+	void insert(Int val) {
+		std::size_t p = 0;
+		for (auto bit : BitHighTraverser(val)) {
+			if (!node(p).next_[bit]) {
+				auto next = alloc();
+				node(p).next_[bit] = next;
+			}
+			p = node(p).next_[bit];
+			node(p).count_++;
+		}
+	}
 
-    void insert(int val) {
-        int p = 0;
-        tree[p].size++;
-        for (int i = Bit - 1; i >= 0; i--) {
-            int bit = (val >> i) & 1;
-            if (!tree[p].son_idx[bit]) tree[p].son_idx[bit] = ++cnt;
-            p = tree[p].son_idx[bit];
-            tree[p].size++;
-        }
-    }
+	std::size_t search(Int val) const {
+		std::size_t p = 0;
+		for (auto bit : BitHighTraverser(val)) {
+			if (!node(p).next_[bit]) {
+				return 0;
+			}
+			p = node(p).next_[bit];
+		}
 
-    int query(int val) const {
-        int p = 0;
-        for (int i = Bit - 1; i >= 0; i--) {
-            int bit = (val >> i) & 1;
-            if (!tree[p].son_idx[bit]) return 0;
-            p = tree[p].son_idx[bit];
-        }
-        return tree[p].size;
-    }
+		return node(p).count_;
+	}
 
-    int maxXor(int val) const {
-        int res = 0, p = 0;
-        for (int i = Bit - 1; i >= 0; i--) {
-            int bit = (val >> i) & 1;
-            if (tree[p].son_idx[bit ^ 1]) {
-                res += 1 << i;
-                bit ^= 1;
-            }
-            p = tree[p].son_idx[bit];
-        }
-        return res;
-    }
+	Int max_xor(Int val) const {
+		U res = 0;
+		std::size_t p = 0;
+		for (auto bit : BitHighTraverser(val)) {
+			res <<= 1;
+			if (node(p).next_[bit ^ 1]) {
+				res |= 1;
+				bit ^= 1;
+			}
+			p = node(p).next_[bit];
+		}
 
-private:
-    std::vector<Node> tree;
-    int cnt{};
+		return static_cast<Int>(res);
+	}
 };
 
+template < std::integral Int >
+class AllXor : private Nodes<AllXorNode> {
+	using Base = Nodes<AllXorNode>;
+	using U    = std::make_unsigned_t<Int>;
 
-template <size_t Bit = 31>
-struct AllXor
-{
-    struct Node
-    {
-        std::array<int, 2> son_idx{};
-        int size{};
-        int xor_val{};
-    };
+	void merge(std::size_t p) {
+		node(p).count_ = node(p).xor_val_ = 0;
+		if (node(p).next_[0]) {
+			node(p).count_ += node(node(p).next_[0]).count_;
+			node(p).xor_val_ ^= node(node(p).next_[0]).xor_val_ << 1;
+		}
+		if (node(p).next_[1]) {
+			node(p).count_ += node(node(p).next_[1]).count_;
+			node(p).xor_val_ ^= node(node(p).next_[1]).xor_val_ << 1 | (node(node(p).next_[1]).count_ & 1);
+		}
+	}
 
-    AllXor(size_t size = 200'000) : tree(size * Bit, Node{}), cnt{0} {}
+	void insert_impl(std::size_t p, Int x, unsigned char depth) {
+		if (depth >= sizeof(Int) * 8) {
+			node(p).count_++;
+			return;
+		}
+		if (node(p).next_[x & 1] == 0) {
+			auto next = alloc();
+			node(p).next_[x & 1] = next;
+		}
+		insert_impl(node(p).next_[x & 1], x >> 1, depth + 1);
+		merge(p);
+	}
 
-    void xorSon(int idx) {
-        tree[idx].size = tree[idx].xor_val = 0;
-        if (tree[idx].son_idx[0]) {
-            tree[idx].size += tree[tree[idx].son_idx[0]].size;
-            tree[idx].xor_val ^= tree[tree[idx].son_idx[0]].xor_val << 1;
-        }
-        if (tree[idx].son_idx[1]) {
-            tree[idx].size += tree[tree[idx].son_idx[1]].size;
-            tree[idx].xor_val ^= tree[tree[idx].son_idx[1]].xor_val << 1 | (tree[tree[idx].son_idx[1]].size & 1);
-        }
-        tree[idx].size &= 1;
-    }
+	void erase_impl(std::size_t p, Int x, unsigned char depth) {
+		if (depth >= sizeof(Int) * 8) {
+			node(p).count_--;
+			return;
+		}
+		erase_impl(node(p).next_[x & 1], x >> 1, depth + 1);
+		merge(p);
+	}
+	
+	void add_impl(std::size_t p) {
+		std::swap(node(p).next_[0], node(p).next_[1]);
+		if (node(p).next_[0]) {
+			add_impl(node(p).next_[0]);
+		}
+		merge(p);
+	}
 
-    void insert(int val) {
-        std::vector<int> stack;
-        stack.reserve(Bit + 1);
-        int now = 0;
-        stack.push_back(now);
-        for (int i = 0; i < (long)(Bit); i++) {
-            int bit = (val >> i) & 1;
-            if (tree[now].son_idx[bit] == 0) tree[now].son_idx[bit] = ++cnt;
-            now = tree[now].son_idx[bit];
-            stack.push_back(now);
-        }
-        tree[stack.back()].size++;
-        stack.pop_back();
-        while(stack.size()) {
-            xorSon(stack.back());
-            stack.pop_back();
-        }
-    }
+public:
+	void insert(Int val) {
+		insert_impl(0, val, 0);
+	}
 
-    int search(int val) const {
-        int now = 0;
-        for (int i = 0; i < Bit; i++) {
-            int bit = (val >> i) & 1;
-            if (!tree[now].son_idx[bit]) return 0;
-            now = tree[now].son_idx[bit];
-        }
-        return tree[now].size;
-    }
+	std::size_t search(Int val) const {
+		std::size_t p = 0;
+		for (auto bit : BitLowTraverser(val)) {
+			if (!node(p).next_[bit]) {
+				return 0;
+			}
+			p = node(p).next_[bit];
+		}
 
-    void erase(int val) {
-        if (search(val) == 0)
-            return;
-        std::vector<int> stack;
-        stack.reserve(Bit + 1);
-        int now = 0;
-        stack.push_back(now);
-        for (int i = 0; i < Bit; i++) {
-            int bit = (val >> i) & 1;
-            now = tree[now].son_idx[bit];
-            stack.push_back(now);
-        }
-        tree[stack.back()].size--;
-        stack.pop_back();
-        while (tree.size()) {
-            xorSon(stack.back());
-            stack.pop_back();
-        }
-    }
+		return node(p).count_;
+	}
 
-    void add() {
-        std::vector<int> stack;
-        stack.reserve(Bit + 1);
-        int now = 0;
-        std::swap(tree[now].son_idx[0], tree[now].son_idx[1]);
-        stack.push_back(now);
-        while(tree[now].son_idx[0]) {
-            now = tree[now].son_idx[0];
-            std::swap(tree[now].son_idx[0], tree[now].son_idx[1]);
-            stack.push_back(now);
-        }
-        while(stack.size()) {
-            xorSon(stack.back());
-            stack.pop_back();
-        }
-    }
+	void erase(Int val) {
+		if (search(val) == 0) {
+			return;
+		}
+		erase_impl(0, val, 0);
+	}
 
-    int allXorValue() const {
-        return tree[0].xor_val;
-    }
-private:
-    std::vector<Node> tree;
-    int cnt = 0;
+	void add() {
+		add_impl(0);
+	}
+
+	Int all_xor() const {
+		return node(0).xor_val_;
+	}
 };
-
-} // namespace Trie
+}
