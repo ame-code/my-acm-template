cfc7c1b6eaf0bbebb9053718cd27f6d9acff4926
reed <1457882960@qq.com>
2026-05-21 00:33:58 +0800

    改进ConvexHull

--- include/acm/CompGeo/ConvexHull.hpp
+++ include/acm/CompGeo/ConvexHull.hpp
@@ -1,8 +1,18 @@
 #pragma once
 #include "../TypeDef.hpp"
+#include "Point.hpp"
+#include "Vector2D.hpp"
+#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <type_traits>
 
-std::vector<Point> getConvexHull(std::vector<Point> points, bool keep_collinear = false) {
-    std::ranges::sort(points);
+template < typename T,
+           typename CrossType = std::conditional_t<std::is_integral_v<T>, long long, T> >
+std::vector<Point<T>> getConvexHull(std::vector<Point<T>> points, bool keep_collinear = false) {
+    std::ranges::sort(points, [](const Point<T>& lhs, const Point<T>& rhs) -> bool {
+        return std::tie(lhs.x_, lhs.y_) < std::tie(rhs.x_, rhs.y_);
+    });
     auto erase_range = std::ranges::unique(points);
     points.erase(erase_range.begin(), erase_range.end());
 
@@ -10,18 +20,15 @@ std::vector<Point> getConvexHull(std::vector<Point> points, bool keep_collinear 
     if (n <= 1) {
         return points;
     }
-    std::vector<Point> lower;
+    std::vector<Point<T>> lower;
     for (int i = 0; i < n; i++) {
         while (ssize(lower) >= 2) {
-            auto cross_multi_res = cross_multi(
-                lower.back() - lower.at(ssize(lower) - 2),
-                points.at(i) - lower.back()
-            );
+            auto cross_val = (lower.back() - lower.at(std::ssize(lower) - 2)).template cross<CrossType>(points.at(i) - lower.back());
             if (keep_collinear) {
-                if (cross_multi_res < 0) lower.pop_back();
+                if (cross_val < 0) lower.pop_back();
                 else break;
             } else {
-                if (cross_multi_res <= 0) lower.pop_back();
+                if (cross_val <= 0) lower.pop_back();
                 else break;
             }
         }
@@ -29,18 +36,15 @@ std::vector<Point> getConvexHull(std::vector<Point> points, bool keep_collinear 
         lower.push_back(points[i]);
     }
 
-    std::vector<Point> upper;
+    std::vector<Point<T>> upper;
     for (int i = n - 1; i >= 0; i--) {
         while (ssize(upper) >= 2) {
-            auto cross_multi_res = cross_multi(
-                upper.back() - upper.at(ssize(upper) - 2),
-                points.at(i) - upper.back()
-            );
+            auto cross_val = (upper.back() - upper.at(std::ssize(upper) - 2)).template cross<CrossType>(points.at(i) - upper.back());
             if (keep_collinear) {
-                if (cross_multi_res < 0) upper.pop_back();
+                if (cross_val < 0) upper.pop_back();
                 else break;
             } else {
-                if (cross_multi_res <= 0) upper.pop_back();
+                if (cross_val <= 0) upper.pop_back();
                 else break;
             }
         }
@@ -48,22 +52,11 @@ std::vector<Point> getConvexHull(std::vector<Point> points, bool keep_collinear 
         upper.push_back(points[i]);
     }
 
-    if(!lower.empty()) lower.pop_back();
-    if(!upper.empty()) upper.pop_back();
-    std::vector<Point> hull(lower.size() + upper.size());
+    if (!lower.empty()) lower.pop_back();
+    if (!upper.empty()) upper.pop_back();
+    std::vector<Point<T>> hull(lower.size() + upper.size());
     std::ranges::copy(lower, hull.begin());
     std::ranges::copy(upper, hull.begin() + lower.size());
-
-    if (hull.empty() && !points.empty()) {
-        if (keep_collinear) {
-            return points;
-        } else {
-            std::vector<Point> endpts;
-            endpts.push_back(points.front());
-            if (!(points.front() == points.back())) endpts.push_back(points.back());
-            return endpts;
-        }
-    }
 
     return hull;
 }
